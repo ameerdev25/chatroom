79f940ce93a6d497528b3719572c5b2f07c1b201
ameerdev25 <ameerdev25@gmail.com>
2022-01-29 17:01:04 +0800

    Multiple duplicate chat fix
    Fixed multiple duplicate chat bubble appearing when sending new chat

--- client/src/components/Chatroom/Chatroom.js
+++ client/src/components/Chatroom/Chatroom.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import ChatBubble from '../ChatBubble/ChatBubble';
 
 const Wrapper = styled.div`
@@ -9,7 +9,7 @@ const Wrapper = styled.div`
 
 const ChatAreaWrapper = styled.div`
   background: #9F3548;
-  width: 80%;
+  width: 100%;
   height: 100%;
   padding: 20px;
   box-sizing: border-box;
@@ -20,11 +20,12 @@ const ChatArea = styled.div`
   background: #fff;
   width: 100%;
   height: 95%;
+  overflow: scroll;
 `;
 
 const Input = styled.input`
   height: 5%;
-  width: 90%;
+  width: 70%;
   margin-top: 8px;
   float: left;
   font-family: 'Dongle', sans-serif;
@@ -33,7 +34,7 @@ const Input = styled.input`
 
 const Button = styled.button`
   height: 5%;
-  width: 8%;
+  width: 20%;
   margin-top: 11px;
   float: right;
   font-family: 'Dongle', sans-serif; 
@@ -48,37 +49,17 @@ const Button = styled.button`
   }
 `;
 
-const UserListWrapper = styled.div`
-  height: 100%;
-  width: 20%;
-  background: #9F3548;
-  float: right;
-  padding: 20px;
-  padding-left: 0;
-  box-sizing: border-box;
-`;
-
-const UserList = styled.div`
-  color: #fff;
-  background: #781426;
-  height: 300px;
-`;
-
-const UserListTitle = styled.p`
-  margin: 0;
-  font-size: 35px;
-  color: #fff;
-`;
-
 function Chatroom(props) {
   const location = useLocation();
+
+  const navigate = useNavigate();
 
   const [username] = useState(location.state.username);
   const [room] = useState(location.state.room);
   const [message, setMessage] = useState("");
   const [chatData, setChatData] = useState([]);
 
-  useEffect(() => {
+  useEffect(() => {    
     getSocketData(props.socket);
   }, []);
 
@@ -91,32 +72,30 @@ function Chatroom(props) {
     }
   }
 
-  const getSocketData = (socket) => {    
-    socket.on("message", (data) => {
-      setChatData(arr => [...arr, {username: data.username, room: data.room, message: data.message, time: data.time}]);
-      console.log("New data received");
-    });  
+  const getSocketData = (socket) => {  
+    if(props.socket === undefined) {
+      navigate('/', {replace: false});
+    } else {
+      socket.on("message", (data) => {
+        setChatData(arr => [...arr, {username: data.username, room: data.room, message: data.message, time: data.time}]);
+        console.log("New data received");
+      }); 
+    }      
   }
 
   return(
     <Wrapper>
-    <ChatAreaWrapper>
-      <ChatArea>
-        {chatData.map((chat, index) => {
-         return <ChatBubble key={index} type={chat.username === username ? "self" : "others"} username={chat.username} message={chat.message} time={chat.time}/>
-        })}
-      </ChatArea>
-      <form onSubmit={sendMessage}>
-        <Input placeholder='Message' value={message} onChange={e => setMessage(e.target.value)}/>
-        <Button>Send</Button>
-      </form>        
-    </ChatAreaWrapper>
-    <UserListWrapper>
-      <UserListTitle>Users</UserListTitle>
-      <UserList>
-        
-      </UserList>
-    </UserListWrapper>
+      <ChatAreaWrapper>
+        <ChatArea>
+          {chatData.map((chat, index) => {
+          return <ChatBubble key={index} type={chat.username === username ? "self" : "others"} username={chat.username} message={chat.message} time={chat.time}/>
+          })}
+        </ChatArea>
+        <form onSubmit={sendMessage}>
+          <Input placeholder='Message' value={message} onChange={e => setMessage(e.target.value)}/>
+          <Button>Send</Button>
+        </form>        
+      </ChatAreaWrapper>
     </Wrapper>
   );
 }
