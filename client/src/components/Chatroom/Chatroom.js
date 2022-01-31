72a56bd5ea0803181387d98a5c74382c885fb102
ameerdev25 <ameerdev25@gmail.com>
2022-01-31 14:15:43 +0800

    Scroll to bottom and emoji
    - Added ability to automatically scroll to new message
    - Added emoji feature to non mobile device (big screen)

--- client/src/components/Chatroom/Chatroom.js
+++ client/src/components/Chatroom/Chatroom.js
@@ -1,6 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import ChatBubble from '../ChatBubble/ChatBubble';
+import { MdSend } from 'react-icons/md';
+import { BsEmojiSmileFill } from 'react-icons/bs'
+import EmojiPicker from 'emoji-picker-react';
 
 function Chatroom(props) {
   const location = useLocation();
@@ -11,6 +14,9 @@ function Chatroom(props) {
   const [room] = useState(location.state.room);
   const [message, setMessage] = useState("");
   const [chatData, setChatData] = useState([]);
+  const [emojiPickerShow, setEmojiPickerShow] = useState(false);
+
+  const scrollPoint = useRef();
 
   useEffect(() => {    
     getSocketData(props.socket);
@@ -31,33 +37,55 @@ function Chatroom(props) {
     } else {
         socket.on("announcement", (data) => {
             setChatData(arr => [...arr, {username: data.username, room: data.room, type: "connect"}])
+            //Scroll to bottom
+            scrollPoint.current.scrollIntoView({behavior: 'smooth'});
         });
 
         socket.on("message", (data) => {
             setChatData(arr => [...arr, {username: data.username, room: data.room, message: data.message, time: data.time, type: "chat"}]);
             console.log("New data received");
+            //Scroll to bottom
+            scrollPoint.current.scrollIntoView({behavior: 'smooth'});
         });   
         
         socket.on("userDisconnect", (data) => {
             setChatData(arr => [...arr, {username: data.username, room: data.room, type: "disconnect"}])
+            //Scroll to bottom
+            scrollPoint.current.scrollIntoView({behavior: 'smooth'});
         });
     }      
   }
 
+  const emojiPicker = () => {
+    if (emojiPickerShow === false) {
+      setEmojiPickerShow(true);
+    } else {
+      setEmojiPickerShow(false);
+    }
+  };
+
+  const onEmojiClick = (event, emojiObject) => {
+    setMessage(message + emojiObject.emoji);
+  };
+
   return(
     <div className='flex flex-col h-full items-center justify-center'>
-        <div className='flex flex-col bg-purple-800 h-full w-full p-3 lg:w-3/12 lg:my-3 lg:mx-3 lg:rounded-lg'>
+        <div className='flex flex-col bg-purple-800 h-full w-full p-3'>
             <div className='flex flex-col bg-white rounded-lg w-full grow p-1 overflow-auto'>
                 {chatData.map((chat, index) => {
                     return <ChatBubble key={index} type={chat.type} sender={chat.username === username ? 'self':'others'} username={chat.username} message={chat.message} time={chat.time}/> 
                 })} 
-                             
+                <div ref={scrollPoint}></div>
             </div>
             <form className='flex mt-3' onSubmit={sendMessage}>
-                <input type='text' className='text-xl rounded-lg pl-1 grow' value={message} onChange={e => setMessage(e.target.value)} placeholder='Message...'/>
-                <button className='bg-white border text-purple-800 rounded-lg font-bold ml-3 py-1 px-4 hover:bg-purple-800 hover:text-white'>Send</button>
+                <input type='text' className='text-xl h-10 rounded-lg pl-1 grow' value={message} onChange={e => setMessage(e.target.value)} placeholder='Message...'/>
+                <button type='button' onClick={emojiPicker} className='bg-white border text-yellow-500 text-2xl rounded-lg font-bold ml-3 py-1 px-4 hover:bg-purple-800 hover:text-white hidden xl:inline-block'><BsEmojiSmileFill /></button>
+                <button type='submit' className='bg-white border text-purple-800 text-2xl rounded-lg font-bold ml-3 py-1 px-4 hover:bg-purple-800 hover:text-white'>
+                    <MdSend />
+                </button>
             </form>
-        </div>
+            {emojiPickerShow ? <EmojiPicker onEmojiClick={onEmojiClick} pickerStyle={{alignSelf: 'end', marginTop: '0.75rem', width: '100%'}}/> : ""}
+        </div>        
     </div>
   );
 }
